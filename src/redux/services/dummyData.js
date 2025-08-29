33f5f245b050f818b3dec5ea865e4a9bac3cbb23
RAHULCHATURVEDI000 <144182517+RAHULCHATURVEDI000@users.noreply.github.com>
2025-08-29 18:04:30 +0530

    Create dummyData.js

---
+++ src/redux/services/dummyData.js
@@ -0,0 +1,21 @@
+// src/redux/services/dummyData.js
+export const dummySongs = [
+  {
+    key: "1",
+    title: "Blinding Lights",
+    subtitle: "The Weeknd",
+    images: {
+      coverart: "https://is4-ssl.mzstatic.com/image/thumb/Music124/v4/53/7b/63/537b635e-3cc9-86a0-5078-07df86c0b106/source/400x400bb.jpg"
+    },
+    hub: { actions: [{ uri: "https://www.youtube.com/watch?v=4NRXx6U8ABQ" }] }
+  },
+  {
+    key: "2",
+    title: "Shape of You",
+    subtitle: "Ed Sheeran",
+    images: {
+      coverart: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/99/06/34/990634b6-4513-c0e2-0f35-48f4e64a6c3c/source/400x400bb.jpg"
+    },
+    hub: { actions: [{ uri: "https://www.youtube.com/watch?v=JGwWNGJdvx8" }] }
+  }
+];
